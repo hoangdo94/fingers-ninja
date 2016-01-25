09e751294e7eb2a9426e91a208dcf4fe58309eed
Hoang Do <hoangdo.dh94@gmail.com>
2016-01-25 16:17:55 +0700

    clean up

--- source/server/modules/generate-vodkar-models.js
+++ source/server/modules/generate-vodkar-models.js
@@ -3,51 +3,6 @@ let models = [
     title: 'Little Ninja',
     imageUrl: '/avatars/default.png',
     price: 0
-  },
-  {
-    title: 'Pretty Death',
-    imageUrl: '/avatars/1.png',
-    price: 100
-  },
-  {
-    title: 'Pumpskin Boy',
-    imageUrl: '/avatars/2.png',
-    price: 200
-  },
-  {
-    title: 'Dummy Mummy',
-    imageUrl: '/avatars/3.png',
-    price: 300
-  },
-  {
-    title: 'Baby Wolf',
-    imageUrl: '/avatars/4.png',
-    price: 400
-  },
-  {
-    title: 'Dump Ghost',
-    imageUrl: '/avatars/5.png',
-    price: 500
-  },
-  {
-    title: 'Sir Vampire',
-    imageUrl: '/avatars/6.png',
-    price: 600
-  },
-  {
-    title: 'White Skeleton',
-    imageUrl: '/avatars/7.png',
-    price: 700
-  },
-  {
-    title: 'Dr Frankenstein',
-    imageUrl: '/avatars/8.png',
-    price: 800
-  },
-  {
-    title: 'Friendly Witch',
-    imageUrl: '/avatars/9.png',
-    price: 900
   }
 ];
 
